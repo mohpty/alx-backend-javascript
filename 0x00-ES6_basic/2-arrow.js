01f32a6b462d02bd8702852d0fe09a5feed962b4
Mohammad Fathi <mohfabdou@gmail.com>
2024-06-24 18:22:45 +0300

    solved no.2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+let x = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
+
+export default x;
