f5de909fde0b856c7148fa4a81737b48eac502a6
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-03 00:47:41 +0300

    fixed some issues

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,13 +1,5 @@
 /**
  * Retrieves students in a given location.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @param {String} city - The location.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns
  */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
